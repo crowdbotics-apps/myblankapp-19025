6285896dcb1867051227c1302c0d7e2e0aaf9e2f
Crowdbotics <team@crowdbotics.com>
2020-07-20 03:12:19 +0000

    remove package blueprint-react-native-camera-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,7 +5,6 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
-import Camera281777Navigator from '../features/Camera281777/navigator';
 import BlankScreen181776Navigator from '../features/BlankScreen181776/navigator';
 
 /**
@@ -15,7 +14,6 @@ import BlankScreen181776Navigator from '../features/BlankScreen181776/navigator'
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
-Camera281777: { screen: Camera281777Navigator },
 BlankScreen181776: { screen: BlankScreen181776Navigator },
 
     /** new navigators can be added here */
